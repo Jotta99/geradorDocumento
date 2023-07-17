b10891faf58d4b3e83d0544d640bd83d38ea5329
Jotta99 <joasamorim611@gmail.com>
2023-07-17 16:43:15 -0400

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+// Elementos DOM
+const nameInput = document.getElementById('nameinput')
+const cpfInput = document.getElementById('cpfinput')
+const rgInput = document.getElementById('rginput')
+const cargoInput = document.getElementById('cargoinput')
+const nomeEmpresaInput = document.getElementById('nomeempresainput')
+const cnpjInput = document.getElementById('cnpjinput')
+const enterInput = document.getElementById('enterinput')
+const saidaInput = document.getElementById('saidainput')
+
+
+
