de1c347fb44a5f0a592834c6cfbcfef8d43d273c
Jotta99 <joasamorim611@gmail.com>
2023-07-17 23:45:59 -0300

    update

--- script.js
+++ script.js
@@ -7,6 +7,40 @@ const nomeEmpresaInput = document.getElementById('nomeempresainput')
 const cnpjInput = document.getElementById('cnpjinput')
 const enterInput = document.getElementById('enterinput')
 const saidaInput = document.getElementById('saidainput')
+const buttonPrint = document.getElementById('buttonprint')
+const buttonView = document.getElementById('buttonview')
+
+// Impressão
+// Elementos DOM
+const nameInputPreenchido = document.getElementById('nomepreenchido')
+const cpfInputPreenchido = document.getElementById('cpfpreenchido')
+const rgInputPreenchido = document.getElementById('rgpreenchido')
+const cargoInputPreenchido = document.getElementById('cargopreenchido')
+const nomeEmpresaInputPreenchido = document.getElementById('empresapreenchido')
+const cnpjInputPreenchido = document.getElementById('cnpjpreenchido')
+const enterInputPreenchido = document.getElementById('entradapreenchido')
+const saidaInputPreenchido = document.getElementById('saidapreenchido')
+
+// Eventos e Funções
 
 
 
+buttonView.addEventListener('click', () => {
+    if(nameInput.value === '' | cpfInput.value === '' |
+    rgInput.value === '' | cargoInput.value === '' |
+    nomeEmpresaInput.value === '' | cnpjInput.value === '' |
+    nameInput.value === '' | nameInput.value === ''){
+        alert('Campos em branco!')
+    }
+    else {
+    nameInputPreenchido.textContent = nameInput.value
+    cpfInputPreenchido.textContent = cpfInput.value
+    rgInputPreenchido.textContent = rgInput.value
+    cargoInputPreenchido.textContent = cargoInput.value
+    nomeEmpresaInputPreenchido.textContent = nomeEmpresaInput.value
+    cnpjInputPreenchido.textContent = cnpjInput.value
+    window.print()
+}
+})
+
+console.log(rgInputPreenchido.textContent)
