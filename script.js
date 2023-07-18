4e8e717764966c0b8f860c54c9b109d468d92bf5
Jotta99 <joasamorim611@gmail.com>
2023-07-18 10:21:01 -0400

    update

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ const nomeEmpresaInput = document.getElementById('nomeempresainput')
 const cnpjInput = document.getElementById('cnpjinput')
 const cityInput = document.getElementById('cityinput')
 const ufInput = document.getElementById('ufinput')
+const dateInput = document.getElementById('dateinput')
 const enterInput = document.getElementById('enterinput')
 const saidaInput = document.getElementById('saidainput')
 const buttonPrint = document.getElementById('buttonprint')
 const buttonView = document.getElementById('buttonview')
+const date = new Date()
+const day = date.getDate()
+const month = date.getMonth()
+const year = date.getFullYear()
+const arrMonth = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+console.log(year)
 
 // Impressão
 // Elementos DOM
@@ -21,12 +29,11 @@ const cargoInputPreenchido = document.getElementById('cargopreenchido')
 const nomeEmpresaInputPreenchido = document.getElementById('empresapreenchido')
 const cnpjInputPreenchido = document.getElementById('cnpjpreenchido')
 const localInputPreenchido = document.getElementById('location')
+const dataInputPreenchido = document.getElementById('datapreenchida')
 const enterInputPreenchido = document.getElementById('entradapreenchido')
 const saidaInputPreenchido = document.getElementById('saidapreenchido')
 
 // Eventos e Funções
-
-
 
 buttonPrint.addEventListener('click', () => {
     if(nameInput.value === '' | cpfInput.value === '' |
@@ -42,9 +49,10 @@ buttonPrint.addEventListener('click', () => {
     cargoInputPreenchido.textContent = cargoInput.value
     nomeEmpresaInputPreenchido.textContent = nomeEmpresaInput.value
     cnpjInputPreenchido.textContent = cnpjInput.value
-    localInputPreenchido.textContent = cityInput.value + ', ' + ufInput.value
+    localInputPreenchido.textContent = cityInput.value + ', ' + ufInput.value + ', ' + day + ' de ' + arrMonth[month] + ' de ' + year
     enterInputPreenchido.textContent = enterInput.value
-    saidaInputPreenchido.textContent = saidaInput.value 
+    saidaInputPreenchido.textContent = saidaInput.value
+    dataInputPreenchido.textContent = dateInput.value
     window.print()
 }
 })
