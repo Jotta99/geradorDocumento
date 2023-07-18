2b2ec17c9d6910f558fadca8bffaffbf2e72b9c1
Jotta99 <joasamorim611@gmail.com>
2023-07-18 09:28:34 -0400

    update

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const rgInput = document.getElementById('rginput')
 const cargoInput = document.getElementById('cargoinput')
 const nomeEmpresaInput = document.getElementById('nomeempresainput')
 const cnpjInput = document.getElementById('cnpjinput')
+const cityInput = document.getElementById('cityinput')
+const ufInput = document.getElementById('ufinput')
 const enterInput = document.getElementById('enterinput')
 const saidaInput = document.getElementById('saidainput')
 const buttonPrint = document.getElementById('buttonprint')
@@ -18,6 +20,7 @@ const rgInputPreenchido = document.getElementById('rgpreenchido')
 const cargoInputPreenchido = document.getElementById('cargopreenchido')
 const nomeEmpresaInputPreenchido = document.getElementById('empresapreenchido')
 const cnpjInputPreenchido = document.getElementById('cnpjpreenchido')
+const localInputPreenchido = document.getElementById('location')
 const enterInputPreenchido = document.getElementById('entradapreenchido')
 const saidaInputPreenchido = document.getElementById('saidapreenchido')
 
@@ -25,7 +28,7 @@ const saidaInputPreenchido = document.getElementById('saidapreenchido')
 
 
 
-buttonView.addEventListener('click', () => {
+buttonPrint.addEventListener('click', () => {
     if(nameInput.value === '' | cpfInput.value === '' |
     rgInput.value === '' | cargoInput.value === '' |
     nomeEmpresaInput.value === '' | cnpjInput.value === '' |
@@ -39,6 +42,9 @@ buttonView.addEventListener('click', () => {
     cargoInputPreenchido.textContent = cargoInput.value
     nomeEmpresaInputPreenchido.textContent = nomeEmpresaInput.value
     cnpjInputPreenchido.textContent = cnpjInput.value
+    localInputPreenchido.textContent = cityInput.value + ', ' + ufInput.value
+    enterInputPreenchido.textContent = enterInput.value
+    saidaInputPreenchido.textContent = saidaInput.value 
     window.print()
 }
 })
